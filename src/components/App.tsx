f0e59da269780b7f4dc6b6df1d999c49de926c72
sumpson0-0 <sumpson00@gmail.com>
2020-10-22 17:36:49 +0900

    Firebase 설정

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Router from "./Router";
+import { authService } from "../fbase";
 
 function App() {
+  console.log(authService.currentUser);
   return (
     <div className="App">
       <Router />
