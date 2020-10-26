8808ea776111f72291959336a80da30856b97788
sumpson0-0 <sumpson00@gmail.com>
2020-10-26 15:05:40 +0900

    Sign In / Sign Up with Email

--- src/routes/Auth.tsx
+++ src/routes/Auth.tsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import { authService } from "../fbase";
 
 const Auth = () => {
-  return <p>Auth</p>;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [toggleAccount, setToggleAccount] = useState<boolean>(false);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement> | any): void => {
+    e.preventDefault();
+    const {
+      target: {
+        2: { value },
+      },
+    } = e;
+    if (value === "Sign Up") {
+      authService.createUserWithEmailAndPassword(email, password);
+    } else if (value === "Log In") {
+      authService.signInWithEmailAndPassword(email, password);
+    }
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const {
+      target: { value: inputValue },
+    } = e;
+    if (e.target.type === "email") {
+      setEmail(inputValue);
+    } else if (e.target.type === "password") {
+      setPassword(inputValue);
+    }
+  };
+
+  const onClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    setToggleAccount((prev) => !prev);
+  };
+
+  return (
+    <>
+      <div>
+        <form onSubmit={onSubmit}>
+          <input
+            type="email"
+            value={email}
+            placeholder="email"
+            onChange={onChange}
+            required
+          />
+          <input
+            type="password"
+            value={password}
+            placeholder="password"
+            onChange={onChange}
+            required
+          />
+          <input type="submit" value={toggleAccount ? "Log In" : "Sign Up"} />
+        </form>
+      </div>
+      <button onClick={onClick}>{toggleAccount ? "Sign Up" : "Log In"}</button>
+    </>
+  );
 };
 
 export default Auth;
