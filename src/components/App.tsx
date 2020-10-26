7e7386b1301fda0ef837037319405ad5004fa725
sumpson0-0 <sumpson00@gmail.com>
2020-10-26 15:06:16 +0900

    로그인 캐치 및 로그인에 따른 Routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Router from "./Router";
 import { authService } from "../fbase";
 
 function App() {
-  console.log(authService.currentUser);
+  const [user, setUser] = useState<object | null>(null);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [init, setInit] = useState<boolean>(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((loggedUser: object | null): void => {
+      if (loggedUser) {
+        setIsLoggedIn(true);
+        setUser(loggedUser);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  });
+
   return (
-    <div className="App">
-      <Router />
-    </div>
+    <>{init ? <Router user={user} isLoggedIn={isLoggedIn} /> : "초기화중..."}</>
   );
 }
 
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,18 +1,37 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
-const Routes = () => {
+interface IProps {
+  user: object | null;
+  isLoggedIn: boolean;
+}
+
+const Routes = ({ user, isLoggedIn }: IProps) => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/auth">
-          <Auth />
-        </Route>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Redirect from="/*" to="/" />
+          </>
+        ) : (
+          <>
+            <Route path="/auth">
+              <Auth />
+            </Route>
+            <Redirect from="/*" to="/auth" />
+          </>
+        )}
       </Switch>
     </Router>
   );
