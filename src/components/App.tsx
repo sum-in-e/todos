b85450d80c0f31536b21036637803947eb22a621
Kim Sumin <66582899+sumpson0-0@users.noreply.github.com>
2020-11-12 21:50:56 +0900

    Alignment

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,62 +1,50 @@
-import React, { useState, useEffect } from "react";
-import Router from "./Router";
-import { authService } from "../fbase";
+import React, { useState, useEffect } from 'react';
+import Router from './Router';
+import { authService } from '../fbase';
 
 interface IUser {
-  uid: string | null;
-  displayName: string | null;
-  updateProfile: (args: object) => void;
+	uid: string | null;
+	displayName: string | null;
+	updateProfile: (args: Object) => void;
 }
 
 function App() {
-  const [userInfo, setUserInfo] = useState<IUser>({
-    uid: null,
-    displayName: null,
-    updateProfile: (args) => args,
-  });
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  const [init, setInit] = useState<boolean>(false);
+	const [userInfo, setUserInfo] = useState<IUser>({
+		uid: null,
+		displayName: null,
+		updateProfile: args => args,
+	});
+	const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+	const [init, setInit] = useState<boolean>(false);
 
-  useEffect(() => {
-    authService.onAuthStateChanged((loggedUser: firebase.User | null): void => {
-      if (loggedUser) {
-        setUserInfo({
-          uid: loggedUser.uid,
-          displayName: loggedUser.displayName,
-          updateProfile: (args) => loggedUser.updateProfile(args),
-        });
-        setIsLoggedIn(true);
-      } else {
-        setIsLoggedIn(false);
-      }
-      setInit(true);
-    });
-  }, []);
+	useEffect(() => {
+		authService.onAuthStateChanged((loggedUser: firebase.User | null): void => {
+			if (loggedUser) {
+				setUserInfo({
+					uid: loggedUser.uid,
+					displayName: loggedUser.displayName,
+					updateProfile: args => loggedUser.updateProfile(args),
+				});
+				setIsLoggedIn(true);
+			} else {
+				setIsLoggedIn(false);
+			}
+			setInit(true);
+		});
+	}, []);
 
-  const reRender = (): void => {
-    const loggedUser: firebase.User | null = authService.currentUser;
-    if (loggedUser) {
-      setUserInfo({
-        uid: loggedUser.uid,
-        displayName: loggedUser.displayName,
-        updateProfile: (args) => loggedUser.updateProfile(args),
-      });
-    }
-  };
+	const reRender = (): void => {
+		const loggedUser: firebase.User | null = authService.currentUser;
+		if (loggedUser) {
+			setUserInfo({
+				uid: loggedUser.uid,
+				displayName: loggedUser.displayName,
+				updateProfile: args => loggedUser.updateProfile(args),
+			});
+		}
+	};
 
-  return (
-    <>
-      {init ? (
-        <Router
-          userInfo={userInfo}
-          isLoggedIn={isLoggedIn}
-          reRender={reRender}
-        />
-      ) : (
-        "초기화중..."
-      )}
-    </>
-  );
+	return <>{init ? <Router userInfo={userInfo} isLoggedIn={isLoggedIn} reRender={reRender} /> : '초기화중...'}</>;
 }
 
 export default App;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,22 +1,23 @@
-import React from "react";
-import MyProfile from "../components/MyProfile";
+import React from 'react';
+import MyProfile from '../components/MyProfile';
+import Tasks from '../components/Tasks';
 
 interface IProps {
-  userInfo: {
-    uid: string | null;
-    displayName: string | null;
-    updateProfile: (args: object) => void;
-  };
-  reRender: () => void;
+	userInfo: {
+		uid: string | null;
+		displayName: string | null;
+		updateProfile: (args: object) => void;
+	};
+	reRender: () => void;
 }
 
-const Home = ({ userInfo, reRender }: IProps) => {
-  return (
-    <>
-      <p>Home</p>
-      <MyProfile userInfo={userInfo} reRender={reRender} />
-    </>
-  );
+const Home: React.FunctionComponent<IProps> = ({ userInfo, reRender }) => {
+	return (
+		<>
+			<MyProfile userInfo={userInfo} reRender={reRender} />
+			<Tasks userInfo={userInfo} />
+		</>
+	);
 };
 
 export default Home;
