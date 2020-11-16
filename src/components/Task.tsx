05165d25880c17614e2456108526548b3d368a44
Kim Sumin <66582899+sumpson0-0@users.noreply.github.com>
2020-11-16 21:38:06 +0900

    ToggleEdit Event

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Edit } from 'styled-icons/boxicons-regular';
 
 interface IProps {
 	task: string;
 }
 
 const Task: React.FunctionComponent<IProps> = ({ task }) => {
+	const [inputValue, setInputValue] = useState<string>(task);
+	const [toggleEdit, setToggleEdit] = useState<boolean>(false);
+
+	const onInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+		const {
+			target: { value },
+		} = e;
+		setInputValue(value);
+	};
+
+	const onToggleClick = (): void => {
+		setToggleEdit(prev => !prev);
+	};
+
 	return (
 		<>
-			<div>{task}</div>
-			<EditIcon />
+			{toggleEdit ? (
+				<>
+					<input type="text" value={inputValue} onChange={onInputChange} placeholder="Edit Task" required />
+					<button>저장</button>
+					<button onClick={onToggleClick}>취소</button>
+				</>
+			) : (
+				<Container>
+					<div>{task}</div>
+					<button onClick={onToggleClick}>수정</button>
+				</Container>
+			)}
 		</>
 	);
 };
 
-const EditIcon = styled(Edit)`
-	width: 20px;
-`;
+const Container = styled.div``;
 
 export default Task;
