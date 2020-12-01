a3cbfa23a93b48360fab4d282ed98a1ef10b56f5
Kim Sumin <66582899+sumpson0-0@users.noreply.github.com>
2020-12-01 12:24:58 +0900

    모바일 사이즈 반응형 px 지정

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -4,9 +4,20 @@ import { normalize } from 'styled-normalize';
 export const GlobalStyle = createGlobalStyle`
   ${normalize}
 
+  @font-face {
+    font-family: 'Eoe_Zno_L';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_eight@1.0/Eoe_Zno_L.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
   html,
   body {
+    font-family: 'Eoe_Zno_L';
     overflow: hidden;
+    ${({ theme }) => theme.media.mobile`
+		font-size : 16px;
+			`}
   }
 
   * {
