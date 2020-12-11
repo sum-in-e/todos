d880424a9aca5a33617ba1c40e456a4a0a9684d9
Kim Sumin <66582899+sumpson0-0@users.noreply.github.com>
2020-12-11 15:54:41 +0900

    완료 탭 전체 비우기 기능 추가

--- src/components/TaskContainer.tsx
+++ src/components/TaskContainer.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { defualtFirebase, dbService } from '../fbase';
 import { v4 as uuidv4 } from 'uuid';
 import Task from './Task';
 import CompletedTask from './CompletedTask';
@@ -23,6 +24,19 @@ const TaskContainer: React.FunctionComponent<IProps> = ({ date, tasks, userInfo,
 	const yyyy = today.getFullYear();
 	const todaysDate = `${yyyy}-${mm < 10 ? `0${mm}` : mm}-${dd < 10 ? `0${dd}` : dd}`;
 
+	const onClearClick = async (): Promise<void> => {
+		if (userInfo.uid !== null) {
+			const completedDoc = dbService.doc(`${userInfo.uid}/완료`);
+			try {
+				await completedDoc.delete();
+			} catch (err) {
+				alert(err.message);
+			} finally {
+				getTasks();
+			}
+		}
+	};
+
 	const identifyPast = (): void => {
 		const todayArr = todaysDate.split('-');
 		const changeToday = new Date(parseInt(todayArr[0]), parseInt(todayArr[1]) - 1, parseInt(todayArr[2]));
@@ -43,8 +57,11 @@ const TaskContainer: React.FunctionComponent<IProps> = ({ date, tasks, userInfo,
 
 	return (
 		<Container>
-			<Dates isPast={isPast}>{date === todaysDate ? '오늘' : date}</Dates>
-			<div>
+			<TitleWrapper>
+				<Dates isPast={isPast}>{date === todaysDate ? '오늘' : date}</Dates>
+				{date === '완료' ? <ClearBtn onClick={onClearClick}>비우기</ClearBtn> : ''}
+			</TitleWrapper>
+			<TasksWrapper>
 				{date === '완료'
 					? Object.entries(tasks).map(([taskKey, taskValue]) => (
 							<CompletedTask
@@ -66,15 +83,37 @@ const TaskContainer: React.FunctionComponent<IProps> = ({ date, tasks, userInfo,
 								getTasks={getTasks}
 							/>
 					  ))}
-			</div>
+			</TasksWrapper>
 		</Container>
 	);
 };
-const Container = styled.div``;
+const Container = styled.article`
+	margin: 1rem 0;
+`;
 
-const Dates = styled.h3<{ isPast: boolean }>`
-	color: ${props => (props.isPast ? props.theme.light.greenColor : 'black')};
+const TitleWrapper = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	height: 1.5rem;
+	margin-bottom: 0.5rem;
+`;
+
+const Dates = styled.h4<{ isPast: boolean }>`
+	color: ${props => (props.isPast ? props.theme.light.blackColor : props.theme.light.whiteColor)};
 	font-weight: bold;
 `;
+
+const ClearBtn = styled.button`
+	outline: none;
+	border: 2px solid ${props => props.theme.light.grayColor};
+	border-radius: 15px;
+	background-color: transparent;
+	font-weight: 700;
+	font-size: 0.7rem;
+	color: ${props => props.theme.light.grayColor};
+`;
+
+const TasksWrapper = styled.div``;
 
 export default TaskContainer;
